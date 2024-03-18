ec35cc5e98bbb01ef986c86dfe9c5d714c1ad5c8
Danny <dannylau@Dannys-MacBook-Air.local>
2024-03-18 12:49:42 -0700

    fix path to images webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                include: path.resolve(__dirname, 'src/img'),
+
             },
         ],
     },
